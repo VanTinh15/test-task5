f1af4e9e16e2f5cec3ea62b35ab63adce774b142
VanTinh15 <tinh.nvt.vn@example.com>
2026-04-20 17:15:26 +0700

    fix-bug

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const swiper1 = new Swiper('.carousel-container', {
-        slidesPerView: 'auto',
+        slidesPerView: 1.2,
         spaceBetween: 20,
+
         grabCursor: true,
         loop: false,
+        roundLengths: true,
+
+     
 
         navigation: {
             nextEl: '.next-btn',
@@ -29,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             768: {
                 slidesPerView: 'auto',
+                centeredSlides: false,
                 spaceBetween: 20
             }
         }
@@ -37,9 +42,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper2 = new Swiper('.mySwiper', {
         slidesPerView: "auto",
         spaceBetween: 0,
+        
         speed: 800,
         grabCursor: true,
         watchSlidesProgress: true,
+        roundLengths: true,
+        
 
         pagination: {
             el: ".swiper-pagination",
@@ -55,19 +63,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const menuHeader = document.querySelector('.menu-header');
 
-    menuToggle.addEventListener('click', function() {
-        
+    menuToggle.addEventListener('click', function () {
+
         menuHeader.classList.toggle('active');
 
 
         if (menuHeader.classList.contains('active')) {
-            menuToggle.innerHTML = '&#10005;'; 
+            menuToggle.innerHTML = '&#10005;';
         } else {
-            menuToggle.innerHTML = '&#9776;'; 
+            menuToggle.innerHTML = '&#9776;';
         }
     });
 
-    // Tự động đóng menu khi click vào một liên kết bất kỳ
     const menuLinks = document.querySelectorAll('.menu-header a');
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
@@ -76,6 +83,87 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    //popup của together
+    const participationForm = document.getElementById('participation-form');
+
+    if (participationForm) {
+        participationForm.addEventListener('submit', function (e) {
+
+            e.preventDefault();
+
+            const formContainer = document.querySelector('.modal-right-content');
+            const thanksView = document.getElementById('thanks-view');
+            const msgTopElement = document.getElementById('msg-top');
+            const movingTextPlaceholder = thanksView.querySelector('.moving-text');
+
+            if (formContainer && thanksView) {
+
+                if (msgTopElement && movingTextPlaceholder) {
+                    movingTextPlaceholder.innerText = msgTopElement.innerText;
+                }
+
+                formContainer.style.display = 'none';
+
+                thanksView.classList.remove('hidden');
+                thanksView.style.display = 'flex';
+            }
+        });
+    }
+
+    //js nhảy số
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200;
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+
+            const inc = target / speed;
+
+            if (count < target) {
+
+                counter.innerText = Math.ceil(count + inc);
+
+                setTimeout(updateCount, 10);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
 
 });
+function openForm(button) {
 
+    const gridItem = button.closest('.grid-item');
+
+    const computedStyle = window.getComputedStyle(gridItem);
+
+    let bgImage = computedStyle.backgroundImage;
+
+    let imageUrl = bgImage.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+
+    const modal = document.getElementById("formModal");
+    const modalImg = document.getElementById("modal-display-img");
+    const modalTitle = document.getElementById("event-name");
+
+    const itemTitle = gridItem.querySelector('h3').innerText;
+
+    modalImg.src = imageUrl;
+    modalTitle.innerText = itemTitle;
+    modal.style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("formModal").style.display = "none";
+}
+
+window.onclick = function (event) {
+    const modal = document.getElementById("formModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
